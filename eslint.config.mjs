b97a90f49db953af82d14369b18c604cb7291c05
jaxcon <fulrion@yandex.ru>
2025-10-23 23:03:03 +0700

    ESLint configuration and fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,44 +3,42 @@ import globals from 'globals';
 import reactPlugin from 'eslint-plugin-react';
 
 export default [
-  js.configs.recommended,
-  {
-    ...reactPlugin.configs.flat.recommended,
-    settings: {
-      react: {
-        version: '19.1.1',
-      },
+    js.configs.recommended,
+    {
+        ...reactPlugin.configs.flat.recommended,
+        settings: {
+            react: {
+                version: '19.1.1',
+            },
+        },
     },
-  },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+
+            'no-unused-vars': 'warn',
+            'no-console': 'warn',
+            'no-case-declarations': 'error',
+            'no-useless-catch': 'error',
+        },
     },
-    rules: {
-      // React
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      
-      // JavaScript
-      'no-unused-vars': 'warn',
-      'no-console': 'warn',
-      'no-case-declarations': 'error', // Оставляем как ошибку чтобы пофиксить
-      'no-useless-catch': 'error',     // Оставляем как ошибку чтобы пофиксить
-    },
-  },
-  {
-    ignores: [
-      'build/**',
-      'public/**',
-      'node_modules/**',
-      '*.worker.*',
-      'generate-sitemap.js'
-    ]
-  }
+    {
+        ignores: [
+            'build/**',
+            'public/**',
+            'node_modules/**',
+            '*.worker.*',
+            'generate-sitemap.js'
+        ]
+    }
 ];
